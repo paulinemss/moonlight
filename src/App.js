2e8ee95ed507ce552eb87cc533f6ec0d03907072
paulinemss <masse.pauline@gmail.com>
2020-10-13 12:17:29 +0200

    Begin mobile responsive work

--- src/App.js
+++ src/App.js
@@ -34,46 +34,89 @@ class App extends React.Component {
 
     return (
       <div className='app'>
-        <div className='column column__moon'>
-          <MoonCard
-            moonPhase={this.state.moonPhase}
-          >
-            <h2>{data[this.state.moonPhase]["name"]}</h2>
-            <h3>Focus: {data[this.state.moonPhase]["focus"]}</h3>
-            <p>{data[this.state.moonPhase]["description"]}</p>
-          </MoonCard>
-          <div className='card__wrapper card__nextmoon'>
-            <NextMoon
-              now={this.state.now}
-            /> 
-          </div>
-        </div>
-        <div className='column column__activities'>
-          <div>
-            <Greeting 
-              now={this.state.now}
-            />
-            <TimeWeather 
-              now={this.state.now}
-            />
+
+        <div className='small-screen'>
+          <Greeting 
+            now={this.state.now}
+          />
+          <TimeWeather 
+            now={this.state.now}
+          />
+          <div className='column column__moon'>
+            <MoonCard
+              moonPhase={this.state.moonPhase}
+            >
+              <h2>{data[this.state.moonPhase]["name"]}</h2>
+              <h3>Focus: {data[this.state.moonPhase]["focus"]}</h3>
+              <p>{data[this.state.moonPhase]["description"]}</p>
+            </MoonCard>
             <div className='card__wrapper card__activities'>
-              <Activities 
+                <Activities 
+                  now={this.state.now}
+                  moonPhase={this.state.moonPhase}
+                  todos={data[this.state.moonPhase]["todos"]}
+                />
+              </div>
+            <div className='card__wrapper card__nextmoon'>
+              <NextMoon
                 now={this.state.now}
-                moonPhase={this.state.moonPhase}
-                todos={data[this.state.moonPhase]["todos"]}
-              />
+              /> 
             </div>
           </div>
           <div className='footer'>
-            Built with <TiHeartOutline /> by <a 
-              href='https://github.com/paulinemss' 
-              rel='noopener noreferrer' 
-              target='_blank'
+              Built with <TiHeartOutline /> by <a 
+                href='https://github.com/paulinemss' 
+                rel='noopener noreferrer' 
+                target='_blank'
+              >
+                Pauline Massé
+              </a>.
+            </div>
+        </div>
+
+        <div className='large-screen'>
+          <div className='column column__moon'>
+            <MoonCard
+              moonPhase={this.state.moonPhase}
             >
-              Pauline Massé
-            </a>.
+              <h2>{data[this.state.moonPhase]["name"]}</h2>
+              <h3>Focus: {data[this.state.moonPhase]["focus"]}</h3>
+              <p>{data[this.state.moonPhase]["description"]}</p>
+            </MoonCard>
+            <div className='card__wrapper card__nextmoon'>
+              <NextMoon
+                now={this.state.now}
+              /> 
+            </div>
+          </div>
+          <div className='column column__activities'>
+            <div>
+              <Greeting 
+                now={this.state.now}
+              />
+              <TimeWeather 
+                now={this.state.now}
+              />
+              <div className='card__wrapper card__activities'>
+                <Activities 
+                  now={this.state.now}
+                  moonPhase={this.state.moonPhase}
+                  todos={data[this.state.moonPhase]["todos"]}
+                />
+              </div>
+            </div>
+            <div className='footer'>
+              Built with <TiHeartOutline /> by <a 
+                href='https://github.com/paulinemss' 
+                rel='noopener noreferrer' 
+                target='_blank'
+              >
+                Pauline Massé
+              </a>.
+            </div>
           </div>
         </div>
+
       </div>
     );
   }
