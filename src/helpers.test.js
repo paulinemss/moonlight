161d23758cd724c97eab4eabc082137bc22efa61
paulinemss <masse.pauline@gmail.com>
2020-10-14 13:50:05 +0200

    Add helpers testing

--- src/helpers.test.js
+++ src/helpers.test.js
@@ -1,5 +1,11 @@
 import { getMoonPhase } from './helpers';
 
 test('should return correct moon phase', () => {
+  expect(getMoonPhase(2020, 10, 16)).toBe('newMoon');
   expect(getMoonPhase(2020, 10, 31)).toBe('fullMoon');
+  expect(getMoonPhase(2020, 11, 30)).toBe('fullMoon');
+  expect(getMoonPhase(2020, 12, 29)).toBe('fullMoon');
+  expect(getMoonPhase(2021, 1, 13)).toBe('newMoon');
+  expect(getMoonPhase(2021, 8, 7)).toBe('newMoon');
+  expect(getMoonPhase(2021, 11, 19)).toBe('fullMoon');
 })
